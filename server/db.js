3fa2f453a346a9db2bdc9b6ce402a51dd2ec2612
sawyer1776 <blackcreekproductions@gmail.com>
2023-12-07 16:54:57 -0500

    sort of working connection to mongo going to go a different rout

--- server/db.js
+++ server/db.js
@@ -1,21 +1,51 @@
 require("dotenv").config();
-const mongoose = require('mongoose');
-mongoose.connect(`mongodb://localhost:27017/${process.env.DB_NAME}`);
-console.log(process.env.DB_NAME)
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'ERRRROOOORRRRR:'));
-db.once('open', () => {
-  console.log('connected to', process.env.DB_NAME);
-})
+const { MongoClient } = require('mongodb');
 
-const reviewSchema = new mongoose.Schema({
+async function listDatabases(client) {
+  databasesList = await client.db("sdc_reviews").collection("reviews").find({ "product_id": 6302 }).count();
 
-})
+  console.log("RETURNED", databasesList);
 
-const findData = async () => {
-  console.log('FUNC');
-  const item = await reviews.findOne();
-  console.log(item);
-}
+};
 
-findData();
+async function main() {
+  const uri = `mongodb://localhost:27017/${process.env.DB_NAME}`;
+  const client = new MongoClient(uri);
+  try {
+    await client.connect();
+
+    await listDatabases(client);
+
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await client.close();
+  }
+};
+
+main().catch(console.error);
+
+
+
+
+
+// const mongoose = require('mongoose');
+// mongoose.connect(`mongodb://localhost:27017/${process.env.DB_NAME}`);
+// console.log(process.env.DB_NAME)
+// const db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'ERRRROOOORRRRR:'));
+// db.once('open', () => {
+//   console.log('connected to', process.env.DB_NAME);
+// })
+
+// const reviewSchema = new mongoose.Schema({
+
+// })
+
+// const findData = async () => {
+//   console.log('FUNC');
+//   const item = await reviews.findOne();
+//   console.log(item);
+// }
+
+// findData();
